4e5eb1622a6448c3789bab9db3c20f6a8192ca1e
WSTxda <wstxda@gmail.com>
2023-12-06 22:49:43 -0300

    Add account manager screen

--- play-services-core/src/main/kotlin/org/microg/gms/ui/SettingsFragment.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/ui/SettingsFragment.kt
@@ -25,6 +25,10 @@ class SettingsFragment : ResourceSettingsFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         super.onCreatePreferences(savedInstanceState, rootKey)
 
+        findPreference<Preference>(PREF_ACCOUNTS)!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            findNavController().navigate(requireContext(), R.id.accountManagerFragment)
+            true
+        }
         findPreference<Preference>(PREF_CHECKIN)!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
             findNavController().navigate(requireContext(), R.id.openCheckinSettings)
             true
@@ -109,6 +113,7 @@ class SettingsFragment : ResourceSettingsFragment() {
         const val PREF_ABOUT = "pref_about"
         const val PREF_GCM = "pref_gcm"
         const val PREF_CHECKIN = "pref_checkin"
+        const val PREF_ACCOUNTS = "pref_accounts"
     }
 
     init {
