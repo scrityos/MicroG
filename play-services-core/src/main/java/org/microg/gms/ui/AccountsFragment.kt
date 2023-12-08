9dc8c91c23842c6279befa96c5cece1d4b56f79f
WSTxda <wstxda@gmail.com>
2023-12-07 22:13:21 -0300

    Tap to remove account in list

--- play-services-core/src/main/java/org/microg/gms/ui/AccountsFragment.kt
+++ play-services-core/src/main/java/org/microg/gms/ui/AccountsFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceCategory
@@ -26,14 +27,73 @@ class AccountsFragment : PreferenceFragmentCompat() {
 
         findPreference<Preference>("pref_current_accounts_none")?.isVisible = accounts.isEmpty()
         val preferenceCategory = findPreference<PreferenceCategory>("prefcat_current_accounts")
-        accounts.forEach {
+        accounts.forEach { account ->
             Preference(requireContext()).apply {
-                title = it.name
-                icon = ContextCompat.getDrawable(
-                    requireContext(),
-                    R.drawable.ic_google_logo
-                )
+                title = account.name
+                icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_google_logo)
                 preferenceCategory?.addPreference(this)
+
+                setOnPreferenceClickListener {
+                    showConfirmationDialog(account.name)
+                    true
+                }
+            }
+        }
+    }
+
+
+    private fun showConfirmationDialog(accountName: String) {
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+        alertDialogBuilder.apply {
+            setTitle(getString(R.string.dialog_title_remove_account))
+            setMessage(getString(R.string.dialog_message_remove_account))
+            setPositiveButton(getString(R.string.dialog_confirm_button)) { _, _ ->
+                if (removeAccount(accountName)) {
+                    val preferenceCategory =
+                        findPreference<PreferenceCategory>("prefcat_current_accounts")
+                    preferenceCategory?.removeAll()
+                    updateAccountList()
+                } else {
+                    Log.e(TAG, "Failed to remove account: $accountName")
+                }
+            }
+            setNegativeButton(getString(R.string.dialog_cancel_button)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            create().show()
+        }
+    }
+
+    private fun removeAccount(accountName: String): Boolean {
+        val accountManager = AccountManager.get(requireContext())
+        val accounts = accountManager.getAccountsByType(AuthConstants.DEFAULT_ACCOUNT_TYPE)
+
+        val accountToRemove = accounts.firstOrNull { it.name == accountName }
+        accountToRemove?.let {
+            return try {
+                accountManager.removeAccount(it, null, null).result
+            } catch (e: Exception) {
+                false
+            }
+        }
+        return false
+    }
+
+    private fun updateAccountList() {
+        val accountManager = AccountManager.get(requireContext())
+        val accounts = accountManager.getAccountsByType(AuthConstants.DEFAULT_ACCOUNT_TYPE)
+
+        val preferenceCategory = findPreference<PreferenceCategory>("prefcat_current_accounts")
+        accounts.forEach { account ->
+            Preference(requireContext()).apply {
+                title = account.name
+                icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_google_logo)
+                preferenceCategory?.addPreference(this)
+
+                setOnPreferenceClickListener {
+                    showConfirmationDialog(account.name)
+                    true
+                }
             }
         }
     }
