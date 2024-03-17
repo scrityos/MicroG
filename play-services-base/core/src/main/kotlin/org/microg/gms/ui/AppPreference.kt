38ddd1e6eb0b3189d80300f31fe8acbcdb4e4e2f
WSTxda <wstxda@gmail.com>
2024-03-16 21:30:14 -0300

    Added Anddea package detection

--- play-services-base/core/src/main/kotlin/org/microg/gms/ui/AppPreference.kt
+++ play-services-base/core/src/main/kotlin/org/microg/gms/ui/AppPreference.kt
@@ -98,6 +98,8 @@ abstract class AppPreference : Preference {
             packageName.contains(".vanced.android") -> context.getString(R.string.vanced)
             packageName.contains(".revanced.android") -> context.getString(R.string.revanced)
             packageName.contains(".rvx.android") -> context.getString(R.string.revanced_extended)
+            packageName.contains("anddea.youtube") -> context.getString(R.string.revanced_extended)
+            packageName.contains("bill.youtube") -> context.getString(R.string.revanced_extended)
             packageName.contains(".rex.android") -> context.getString(R.string.youtube_advanced)
             else -> ""
         }
